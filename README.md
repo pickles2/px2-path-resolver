f900538e2233073f5ebecb5afa5dac178c4194cb
Tomoya Koyanagi <tomk79@gmail.com>
2015-06-24 02:16:16 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+px2-path-resolver
+=========
+
+_px2-path-resolver_ は、[Pickles 2](http://pickles2.pxt.jp/) に、相対パス・絶対パスでの書き出しオプションを追加します。
+
+
+
+## ライセンス - License
+
+MIT License
+
+
+## 作者 - Author
+
+- (C)Tomoya Koyanagi <tomk79@gmail.com>
+- website: <http://www.pxt.jp/>
+- Twitter: @tomk79 <http://twitter.com/tomk79/>
+
+
---
+++ php/pathResolver.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Pickles2 DEC CORE class
+ */
+namespace tomk79\pickles2\pathResolver;
+
+/**
+ * Pickles2 DEC CORE class
+ */
+class main{
+	private $px, $options;
+
+	/**
+	 * 変換処理の実行
+	 * @param object $px Picklesオブジェクト
+	 */
+	public static function exec( $px, $options = null ){
+		// var_dump($options);
+		(new self($px, $options))->resolve();
+		return true;
+	}
+
+	/**
+	 * constructor
+	 */
+	public function __construct( $px, $options ){
+		$this->px = $px;
+		if( is_string($options) ){
+			$this->options['to'] = $options;
+		}elseif( is_object($options) || is_array($options) ){
+			$this->options = $options;
+		}
+		$this->options = json_decode(json_encode($options));
+		require_once(__DIR__.'/simple_html_dom.php');
+	}
+
+	/**
+	 * resolve path
+	 */
+	public function resolve(){
+
+		$ext = $this->px->fs()->get_extension($this->px->req()->get_request_file_path());
+
+		foreach( $this->px->bowl()->get_keys() as $key ){
+			$src = $this->px->bowl()->pull( $key );
+
+			switch( strtolower($ext) ){
+				case 'html':
+				case 'htm':
+					$src = $this->path_resolve_in_html($src);
+					break;
+				case 'css':
+					$src = $this->path_resolve_in_css($src);
+					break;
+			}
+
+			$this->px->bowl()->replace( $src, $key );
+		}
+		return true;
+	}
+
+	/**
+	 * HTMLファイル中のパスを解決
+	 */
+	private function path_resolve_in_html( $src ){
+
+		// data-dec-blockブロックを削除
+		$html = str_get_html(
+			$src ,
+			false, // $lowercase
+			false, // $forceTagsClosed
+			DEFAULT_TARGET_CHARSET, // $target_charset
+			false, // $stripRN
+			DEFAULT_BR_TEXT, // $defaultBRText
+			DEFAULT_SPAN_TEXT // $defaultSpanText
+		);
+
+		$ret = $html->find('*[href]');
+		foreach( $ret as $retRow ){
+			$val = $retRow->getAttribute('href');
+			$val = $this->get_new_path($val);
+			$retRow->setAttribute('href', $val);
+		}
+
+		$ret = $html->find('*[src]');
+		foreach( $ret as $retRow ){
+			$val = $retRow->getAttribute('src');
+			$val = $this->get_new_path($val);
+			$retRow->setAttribute('src', $val);
+		}
+
+		$ret = $html->find('form[action]');
+		foreach( $ret as $retRow ){
+			$val = $retRow->getAttribute('action');
+			$val = $this->get_new_path($val);
+			$retRow->setAttribute('action', $val);
+		}
+
+		$src = $html->outertext;
+
+		return $src;
+	}
+
+	/**
+	 * CSSファイル中のパスを解決
+	 */
+	private function path_resolve_in_css( $bin ){
+
+		$rtn = '';
+		while( 1 ){
+			if( !preg_match( '/^(.*?)url\s*\\((.*?)\\)(.*)$/si', $bin, $matched ) ){
+				$rtn .= $bin;
+				break;
+			}
+			$rtn .= $matched[1];
+			$rtn .= 'url("';
+			$res = trim( $matched[2] );
+			if( preg_match( '/^(\"|\')(.*)\1$/si', $res, $matched2 ) ){
+				$res = trim( $matched2[2] );
+			}
+			$res = $this->get_new_path( $res );
+			$rtn .= $res;
+			$rtn .= '")';
+			$bin = $matched[3];
+		}
+
+		return $rtn;
+	}
+
+	/**
+	 * 変換後の新しいパスを取得
+	 */
+	private function get_new_path( $path ){
+		if( preg_match( '/^([a-zA-Z0-9]+\:|\/\/)/', $path ) ){
+			return $path;
+		}
+		$cd = $this->px->href( $this->px->req()->get_request_file_path() );
+		$cd = preg_replace( '/^(.*)(\/.*?)$/si', '$1', $cd );
+		if( !strlen($cd) ){
+			$cd = '/';
+		}
+
+		switch(strtolower($this->options->to)){
+			case 'relate':
+				// 相対パスへ変換
+				$path = $this->px->fs()->get_realpath($path, $cd);
+				$path = $this->px->fs()->get_relatedpath($path, $cd);
+				break;
+			case 'absolute':
+				// 絶対パスへ変換
+				$path = $this->px->fs()->get_realpath($path, $cd);
+				break;
+			case 'pass':
+			default:
+				// 処理を行わない
+				break;
+		}
+
+		return $path;
+	}
+
+}
